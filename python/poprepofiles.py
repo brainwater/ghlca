0a25ddaae424b2975d2fb450130389e8dea051f2
Blake Rainwater <rainwaterblake@gmail.com>
2014-08-16 15:05:26 -0400

    Completed pop files

--- python/poprepofiles.py
+++ python/poprepofiles.py
@@ -1,18 +1,35 @@
 import ghlca
 from pathlib import Path
 import re
+import sys
+import json
+import subprocess
 
 
-def popfilenames(coll):
-    return None
-#    for repo in coll.find():
+flistfile = "/tmp/ghlca-file-list.txt"
+infofile = "/tmp/ghlca-lang-info.txt"
 
-def poprepofilenames(reponame, fdir):
+def popfilenames(coll, fdir):
+    for repo in coll.find():
+        if "repository_full_name" not in repo:
+            continue
+        reponame = repo["repository_full_name"]
+        print(reponame)
+        repo["repository_file_list"] = getrepofileinfos(reponame, fdir)
+        coll.save(repo)
+
+def getrepofileinfos(reponame, fdir):
     globber = reponame + "/**/*"
     paths = Path(fdir + "/").glob(globber)
-    for path in paths:
-        if path.is_file():
-            print(path)
+    pathstrs = [str(i) for i in paths if i.is_file()]
+    with open(flistfile, 'w+') as outfile:
+        json.dump(pathstrs, outfile)
+    subprocess.call('ruby ruby/langinfo.rb "' + flistfile + '" "' + infofile + '"', shell=True)
+    finfos = []
+    with open(infofile) as infile:
+        finfos = json.load(infile)
+    return finfos
 
-poprepofilenames("apache/cassandra", ghlca.wfilesdir)
+#print(getrepofileinfos("apache/cassandra", ghlca.wfilesdir))
+popfilenames(ghlca.wcoll, ghlca.wfilesdir)
 
--- ruby/langinfo.rb
+++ ruby/langinfo.rb
@@ -6,10 +6,17 @@ require 'json'
 
 def getFileInfo(f)
   fblob = Linguist::FileBlob.new(f.chomp)
-  language = fblob.language.nil? ? "None" : fblob.language.name
-  info = { "language" => language, "sloc" => fblob.sloc, "loc" => fblob.loc, "vendored" => fblob.vendored?, "generated" => fblob.generated? }
+  info = {}
+  lang = fblob.language
+  if lang.nil?
+    info = { "filename" => f.chomp, "vendored" => fblob.vendored?, "generated" => fblob.generated? }
+  else
+    info = { "filename" => f.chomp, "language" => lang.name, "sloc" => fblob.sloc, "loc" => fblob.loc, "vendored" => fblob.vendored?, "generated" => fblob.generated? }
+  end
+  info
 end
 
 flisttext = File.open(ARGV[0]).read
 fout = File.open(ARGV[1], "w")
-fout.write(flisttext.each_line.collect{|f| getFileInfo(f)}.to_json)
+fparsed = JSON.parse(flisttext)
+fout.write(fparsed.collect{|f| getFileInfo(f)}.to_json)
